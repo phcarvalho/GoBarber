f6b145246329ef0667fbba59597191ae5122d778
Paulo Carvalho <phcarvalhodev@gmail.com>
2020-02-05 00:23:05 -0300

    Added login page styles

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform'
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg'
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
-    <div />
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho uma conta</Link>
+      </Form>
+    </>
   );
 }
