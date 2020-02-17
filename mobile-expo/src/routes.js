0fdcd0e16c214add820bb0a79f2e42b3ea7cc5ca
Paulo Carvalho <phcarvalhodev@gmail.com>
2020-02-17 00:16:57 -0300

    Created mobile app with expo

---
+++ mobile-expo/src/routes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
